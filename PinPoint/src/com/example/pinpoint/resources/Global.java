2200a3ea3d7c790192c225abf6c7e6b6642034ce
ianstavros <ianstavros@10.254.97.184>
2014-03-30 13:21:33 -0400

    created a Hjclient, database and Global java file, needs to be modified to our porgram

---
+++ PinPoint/src/com/example/pinpoint/resources/Global.java
@@ -0,0 +1,56 @@
+package com.example.pinpoint.resources;
+
+import android.app.Application;
+import android.content.Context;
+import android.location.LocationManager;
+
+import com.example.pinpoint.models.User;
+
+import retrofit.RestAdapter;
+
+public class Global {
+	private static final String API_URL = "http://67.165.80.24:8000";
+    private static RestAdapter restAdapter = new RestAdapter.Builder()
+            .setEndpoint(API_URL)
+            .build();
+    private static HJClient client = restAdapter.create(HJClient.class);
+    private static User user = new User();
+
+    private static Context context;
+    private static Database database;
+    private static LocationManager locationManager = null;
+
+    public static User getUser() {
+        return user;
+    }
+    public static void setUser(User newUser) {
+        user = newUser;
+    }
+
+    public static void newInstance(LocationManager locMan) {
+        database = new Database(context);
+        locationManager = locMan;
+    }
+    public static Database getDatabase() {
+        return database;
+    }
+
+    public static void setContext(Context con) {
+        context = con;
+    }
+    public static Context getContext() {
+        return context;
+    }
+
+    public static HJClient getClient() {
+        return client;
+    }
+
+    public static String getRootUrl() {
+        return API_URL;
+    }
+
+    public static LocationManager getLocationManager() {
+        return locationManager;
+    }
+}
---
+++ PinPoint/src/com/example/pinpoint/resources/HJClient.java
@@ -0,0 +1,27 @@
+package com.example.pinpoint.resources;
+
+	import com.example.pinpoint.models.Event;
+	import com.example.pinpoint.models.User;
+
+	import java.util.List;
+
+	import retrofit.Callback;
+	import retrofit.http.Body;
+	import retrofit.http.GET;
+	import retrofit.http.POST;
+
+	/**
+	 * Created by root on 3/26/14.
+	 */
+	public interface HJClient {
+
+	    @POST("/user/login")
+	    void login(
+	            @Body User user,
+	            Callback<User> callback
+	    );
+
+	    @GET("/events")
+	    void events(Callback<List<Event>> callback);
+	}
+}
