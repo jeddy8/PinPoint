1e8d596e4d1f7b90a29a3db71ee9e91cbd16d6e3
Greg <Greg@10.255.179.138>
2014-04-20 15:01:34 -0400

    map works

--- PinPoint/src/com/example/pinpoint/MapActivity.java
+++ PinPoint/src/com/example/pinpoint/MapActivity.java
@@ -1,79 +1,48 @@
 package com.example.pinpoint;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import com.google.android.gms.maps.*;
-import com.google.android.gms.maps.model.*;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends Activity {
-	  static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-	  static final LatLng KIEL = new LatLng(53.551, 9.993);
-	  private GoogleMap map;
+  static final LatLng MTNLAIR = new LatLng(39.635053, -79.954028);
+  static final LatLng ESB = new LatLng(39.645480, -79.973254);
+  private GoogleMap map;
 
-	  @Override
-	  protected void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_main);
-	    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-	        .getMap();
-	    
-	    if (map!=null){
-	      Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-	          .title("Hamburg"));
-	      Marker kiel = map.addMarker(new MarkerOptions()
-	          .position(KIEL)
-	          .title("Kiel")
-	          .snippet("Kiel is cool")
-	          .icon(BitmapDescriptorFactory
-	              .fromResource(R.drawable.ic_launcher)));
-	    }
-	    
-	  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_map);
+    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+        .getMap();
+    Marker mtnlair = map.addMarker(new MarkerOptions().position(MTNLAIR)
+        .title("Mountainlair"));
+    Marker esb = map.addMarker(new MarkerOptions()
+        .position(ESB)
+        .title("ESB")
+        .snippet("description")
+        .icon(BitmapDescriptorFactory
+            .fromResource(R.drawable.ic_launcher)));
 
+    // Move the camera instantly to hamburg with a zoom of 15.
+    map.moveCamera(CameraUpdateFactory.newLatLngZoom(ESB, 20));
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+    // Zoom in, animating the camera.
+    map.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+  }
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.map, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_map, container,
-					false);
-			return rootView;
-		}
-	}
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.map, menu);
+    return true;
+  }
 
 }
